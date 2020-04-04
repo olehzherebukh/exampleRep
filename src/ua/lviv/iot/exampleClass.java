08bfacc6744e9058abf3769dba313939b588c941
olehzherebukh <olehzherebukh@gmail.com>
2020-04-04 14:41:56 +0300

    firstBranch test

--- src/ua/lviv/iot/exampleClass.java
+++ src/ua/lviv/iot/exampleClass.java
@@ -3,5 +3,6 @@ package ua.lviv.iot;
 public class exampleClass {
     public static void main(String[] args) {
         System.out.println("git test");
+        System.out.println("firstBranch test");
     }
 }
