32f81a95f0660ccf02526b1c1de65bbaee98a518
olehzherebukh <olehzherebukh@gmail.com>
2020-04-04 14:53:12 +0300

    trying to make conflict

--- src/ua/lviv/iot/exampleClass.java
+++ src/ua/lviv/iot/exampleClass.java
@@ -2,7 +2,9 @@ package ua.lviv.iot;
 
 public class exampleClass {
     public static void main(String[] args) {
-        System.out.println("git test");
         System.out.println("firstBranch test");
+
+
+        System.out.println("conflict");
     }
 }
